aead24fcdc3f31b9eb30c59b47316585f6511d6e
Onkar Dhane <engg.onkar@gmail.com>
2020-07-10 16:51:38 +0400

    removed venv

--- db/init.sql
+++ db/init.sql
@@ -71,4 +71,44 @@ BEGIN
 END$$
 DELIMITER ;
 
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_deleteWish`(
+IN p_wish_id bigint,
+IN p_user_id bigint
+)
+BEGIN
+delete from tbl_wish where wish_id = p_wish_id and wish_user_id = p_user_id;
+END$$
+DELIMITER ;
 
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_updateWish`(
+IN p_title varchar(45),
+IN p_description varchar(1000),
+IN p_wish_id bigint,
+In p_user_id bigint
+)
+BEGIN
+update tbl_wish set wish_title = p_title,wish_description = p_description
+    where wish_id = p_wish_id and wish_user_id = p_user_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetWishById`(
+IN p_wish_id bigint,
+In p_user_id bigint
+)
+BEGIN
+select * from tbl_wish where wish_id = p_wish_id and wish_user_id = p_user_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetWishByUser`(
+IN p_user_id bigint
+)
+BEGIN
+    select * from tbl_wish where wish_user_id = p_user_id;
+END$$
+DELIMITER ;

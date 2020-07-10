80acd713c92f250f10f3c81346ff5b9ee6ef9795
Onkar Dhane <engg.onkar@gmail.com>
2020-07-10 16:23:12 +0400

    removed venv

---
+++ db/init.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE BucketList;
+
+CREATE TABLE `BucketList`.`tbl_user` (
+  `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `user_username` VARCHAR(45) NOT NULL,
+  `user_password` VARCHAR(245) NOT NULL,
+  PRIMARY KEY (`user_id`));
+
+use BucketList;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
+    IN p_name VARCHAR(20),
+    IN p_username VARCHAR(20),
+    IN p_password VARCHAR(245)
+)
+BEGIN
+    if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
+
+        select 'Username Exists !!';
+
+    ELSE
+
+        insert into tbl_user
+        (
+            user_name,
+            user_username,
+            user_password
+        )
+        values
+        (
+            p_name,
+            p_username,
+            p_password
+        );
+
+    END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_addWish`(
+    IN p_title varchar(45),
+    IN p_description varchar(1000),
+    IN p_user_id bigint
+)
+BEGIN
+    insert into tbl_wish(
+        wish_title,
+        wish_description,
+        wish_user_id,
+        wish_date
+    )
+    values
+    (
+        p_title,
+        p_description,
+        p_user_id,
+        NOW()
+    );
+END$$
+DELIMITER ;
+
+
